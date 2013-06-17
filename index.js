4b196f70f1561f3b5e6b9b75a8f5a1e38bbee384
Enrico Marino <enrico.marino@email.com>
2013-06-17 18:32:36 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * template
+ * Template engine
+ *
+ * @copyright Enrico Marino and Federico Spini
+ * @license MIT
+ */
+
+/**
+ * Expose component
+ */
+
+module.exports = template;
+
+/**
+ * template
+ */
+
+function template () {
+  //TODO
+}
